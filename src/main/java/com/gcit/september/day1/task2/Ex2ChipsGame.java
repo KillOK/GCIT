420492aec0f23f8bca7d611d7b10944f8edf89e1
Stepan_ua <stepantytseyko@gmail.com>
2018-09-20 13:57:14 -0300

    added task file with proj description, created method for max user input calculation, made a small refactoring

--- src/main/java/com/gcit/september/day1/task2/Ex2ChipsGame.java
+++ src/main/java/com/gcit/september/day1/task2/Ex2ChipsGame.java
@@ -57,6 +57,7 @@ class MenuUI {
 		System.out.println(border);
 		System.out.println("Good bue!");
 		System.out.println(border);
+		scn.close();
 	}
 }
 
@@ -67,55 +68,62 @@ class Game {
 
 	private Player[] players = { new Player(), new Player() };	// Array of players
 	
-	int buffer;					// count tries
-	int magicNumber;			// number of chips in the pile 								(depends from user input)
+	int currentChipsNumber;			// number of chips in the pile 								(depends from user input)
 	int yourNumber;				// player choice 			   								(depends from user input)
 	int minPileCapacity = 3;	// minPileCapacity											(depends from user input)
 	int maxPileCapacity = 999;	// maxPileCapacity											(depends from user input)
 	int minPlayerChoice = 1;	// minPlayerChoice											(depends from user input)
-	int maxPlayerChoice;		// maxPlayerChoice - middle value of the pile				(depends from user magicNumber)
+	int middlePileNumber;		// maxPlayerChoice - middle value of the pile				(depends from currentChipsNumber)
 
 	Scanner sc = new Scanner(System.in);
 
 	public Game() {
-		for (Player p : players) {
-			System.out.println("Enter name of " + p.getPlayerId());
-			p.setPlayerName(setNames());
-			System.out.println(p.getPlayerId() + " name: " + p.getPlayerName());
-		}
-		magicNumber = enterInitialChipsNumber();
+		System.out.println(border);
+		initializePlayers();
+		currentChipsNumber = enterInitialChipsNumber();
 		System.out.println(border);
 	}
+	
+	private void initializePlayers() {
+		for (Player p : players) {
+			System.out.println("Enter name of " + p.getPlayerId());
+			p.setPlayerName(inputName());
+			System.out.println(p.getPlayerId() + " name: " + p.getPlayerName());
+			System.out.println(border);
+		}
+	}
 
-	public String setNames() {
+	public String inputName() {
 		String name = null;
 		if (sc.hasNextLine()) {
 			name = sc.nextLine();
-			for (Player player : players) {
-				if (player.getPlayerName() != null)
+			for (Player player : players) {					// Input Validation
+				if (player.getPlayerName() != null) {
 					if (player.getPlayerName().toLowerCase().equals(name.toLowerCase())) {
 						System.out.println("Both players cannot be named " + name + "\nEnter name of Player2");
-						name = setNames();
+						name = inputName();
 						break;
 					}
+				}
 			}
 		}
 		return name;
 	}
 
 	public void start() {
-		while (magicNumber > 0) {
+		while (currentChipsNumber > 0) {
 			for (Player player : players) {
-				if (magicNumber > 0) {
+				if (currentChipsNumber > 0) {
 					for (Player p : players) {
-						System.out.println(p.getPlayerName() + " has " + p.getCookies() + " chips");
+						System.out.println(p.getPlayerName() + " has " + p.getChipsValue() + " chips");
 					}
-					System.out.println("It's your turn " + player.getPlayerName() + " \n" + "There are " + magicNumber
-							+ " chips remaining. You may take any number of chips from 1 to "
-							+ (((magicNumber - 1) / 2) + 1) + "\nHow many will you take, "+player.getPlayerName()+"?");
+					middlePileNumber = calculateMiddleValue();
+					System.out.println("It's your turn " + player.getPlayerName() + " \n" + "There are " + currentChipsNumber
+							+ " chips remaining. You may take any number of chips from "+minPlayerChoice+" to "
+							+ middlePileNumber + "\nHow many will you take, "+player.getPlayerName()+"?");
 					enterNumber();
-					magicNumber -= yourNumber;
-					player.setCookies(player.getCookies() + yourNumber);
+					currentChipsNumber -= yourNumber;
+					player.setChipsValue(player.getChipsValue() + yourNumber);
 					System.out.println(border);
 					
 				}
@@ -126,11 +134,11 @@ class Game {
 
 	public void finish() {
 		for (Player p : players) {
-			System.out.println(p.getPlayerName() + " has " + p.getCookies() + " chips");
+			System.out.println(p.getPlayerName() + " has " + p.getChipsValue() + " chips");
 		}
 		for (Player p : players) {
-			if (p.getCookies() % 2 == 0) {
-				System.out.println(p.getPlayerName() + " has even  number of chips: " + p.getCookies());
+			if (p.getChipsValue() % 2 == 0) {
+				System.out.println(p.getPlayerName() + " has even  number of chips: " + p.getChipsValue());
 				System.out.println(p.getPlayerName() + " wins!");
 			}
 		}
@@ -139,7 +147,7 @@ class Game {
 	
 	/**
 	 * User input validation of initial value of cookies in the pile
-	 * @return the initial value of the cookis
+	 * @return the initial value of the cookies
 	 */
 	public int enterInitialChipsNumber() {
 		 int chipsNumber = 0;
@@ -149,7 +157,7 @@ class Game {
 			if (sc.hasNextInt()) {
 				chipsNumber = sc.nextInt();
 				if (chipsNumber > maxPileCapacity || chipsNumber < minPileCapacity) {
-					System.out.println("You have to start with at least 3 chips and less then "+maxPileCapacity);
+					System.out.println("You have to start with at least "+minPileCapacity+" chips and less then "+maxPileCapacity);
 					chipsNumber = enterInitialChipsNumber();
 				}
 				if (chipsNumber % 2 == 0) {
@@ -157,7 +165,7 @@ class Game {
 					chipsNumber = enterInitialChipsNumber();
 				}
 			}else{
-				System.out.println("You should enter number from 1 to 1000");
+				System.out.println("You should enter number from "+minPileCapacity+" to "+maxPileCapacity);
 				sc.next();
 				chipsNumber = enterInitialChipsNumber();
 			}
@@ -170,18 +178,28 @@ class Game {
 	 * Usage of global variable in this methods is bad practice, but I hope in so small code part it's not a big problem...
 	 */
 	public void enterNumber() {
-		System.out.println("Player remove between 1 and " + (((magicNumber - 1) / 2) + 1) + " from the pile");
+		System.out.println("Player remove between "+minPlayerChoice+" and " + middlePileNumber + " from the pile");
 		if (sc.hasNextInt()) {
 			yourNumber = sc.nextInt();
-			if ((yourNumber > ((magicNumber - 1) / 2) + 1) || yourNumber < 1) {
-				System.out.println("Number should be between 1 and " + (((magicNumber - 1) / 2) + 1));
+			if ((yourNumber > middlePileNumber) || yourNumber < minPlayerChoice) {
+				System.out.println("Number should be between "+minPlayerChoice+" and " + middlePileNumber);
 				enterNumber();
 			}
 		} else {
-			System.out.println("Number should be between 1 and " + (((magicNumber - 1) / 2) + 1));
+			System.out.println("Number should be between "+minPlayerChoice+" and " + middlePileNumber);
 			sc.next();
 			enterNumber();
 		}
+	}
+	
+	/**
+	 * Calculate the middle value of the chips in the pile
+	 * @return the middle value of the chips in the pile
+	 */
+	public int calculateMiddleValue() {
+		if(currentChipsNumber%2==0)return currentChipsNumber/2;
+		else return ((currentChipsNumber-1)/2)+1;
+		
 	}
 }
 
@@ -192,7 +210,7 @@ class Game {
  *	var id{@link Player#id} - counts players
  *	var PlayerId{@link Player#PlayerId} - String PlayerId
  *	var playerName{@link Player#playerName} - name from user input
- *	var cookies{@link Player#cookies} - value of cookies, which the user have
+ *	var cookies{@link Player#chipsValue} - value of cookies, which the user have
  */
 class Player {
 	private static int id = 0;
@@ -211,7 +229,7 @@ class Player {
 
 	private String PlayerId;
 	private String playerName;
-	public int cookies;
+	public int chipsValue;
 	
 	
 	//geters and setters...
@@ -227,12 +245,12 @@ class Player {
 		this.playerName = playerName;
 	}
 
-	public int getCookies() {
-		return cookies;
+	public int getChipsValue() {
+		return chipsValue;
 	}
 
-	public void setCookies(int cookies) {
-		this.cookies = cookies;
+	public void setChipsValue(int chips) {
+		this.chipsValue = chips;
 	}
 
 	public static int getId() {
